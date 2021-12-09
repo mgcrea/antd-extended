7a1dbfd8b52a9187c915008372512fae1a6fd3a7
Olivier Louvignes <olivier@mgcrea.io>
2021-12-09 11:46:34 +0100

    feat(input): update Input API

--- src/input/index.ts
+++ src/input/index.ts
@@ -5,7 +5,11 @@ export * from './Password';
 
 import {Input as BaseInput} from './Input';
 import {Password} from './Password';
+import {Search} from './Search';
+import {TextArea} from './TextArea';
 
 export const Input = Object.assign(BaseInput, {
   Password,
+  Search,
+  TextArea,
 });
