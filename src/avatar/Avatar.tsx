ca078ef54784e5d5da628603665d326b1c270f02
Olivier Louvignes <olivier@mgcrea.io>
2022-01-10 18:04:34 +0100

    feat(avatar): add <Avatar /> type prop

--- src/avatar/Avatar.tsx
+++ src/avatar/Avatar.tsx
@@ -2,15 +2,40 @@
 
 import {Avatar as AntDesignAvatar} from 'antd';
 import {AvatarProps as AntDesignAvatarProps} from 'antd/lib/avatar';
-import {AvatarSize} from 'antd/lib/avatar/SizeContext';
 import React, {FunctionComponent} from 'react';
-import {applySizeProps, SizeType} from './../utils';
+import {applySizeProps, classNames, SizeType} from './../utils';
 import './style/avatar.less';
+
+export type AvatarTypes = 'default' | 'primary' | 'secondary';
 
 export type AvatarProps = Omit<AntDesignAvatarProps, 'size'> & {
   size?: SizeType;
+  type?: AvatarTypes;
 };
 
-export const Avatar: FunctionComponent<AvatarProps> = ({size, className, ...otherProps}) => {
-  return <AntDesignAvatar {...(applySizeProps('ant-avatar', {size, className}) as AvatarSize)} {...otherProps} />;
+export const Avatar: FunctionComponent<AvatarProps> = ({size, className, type, ...otherProps}) => {
+  const {className: sizeClassName = className, ...otherSizeProps} = applySizeProps('ant-avatar', {size, className});
+  return (
+    <AntDesignAvatar
+      {...applyAvatarTypeProps('ant-avatar', {type, className: sizeClassName})}
+      {...(otherSizeProps as AntDesignAvatarProps)}
+      {...otherProps}
+    />
+  );
+};
+
+type AvatarTypeProps = {
+  className?: string;
+  type?: AvatarTypes;
+};
+
+const applyAvatarTypeProps = (prefix: string, {className, type}: AvatarTypeProps): AntDesignAvatarProps => {
+  switch (type) {
+    case 'primary':
+      return {className: classNames(className, `${prefix}-primary`)};
+    case 'secondary':
+      return {className: classNames(className, `${prefix}-secondary`)};
+    default:
+      return {className};
+  }
 };
--- stories/avatar/Avatar.stories.tsx
+++ stories/avatar/Avatar.stories.tsx
@@ -2,17 +2,34 @@
 
 import {ComponentMeta, ComponentStory} from '@storybook/react';
 import React from 'react';
-import {sizeTemplate, titlePrefix} from '../utils';
-import {Avatar} from './../../src/avatar';
+import {declineTemplate, sizeTemplate, storybookSizeArgTypes, titlePrefix} from '../utils';
+import {Avatar, AvatarProps} from '../../src/avatar';
+
+const buttonTypeOptions: AvatarProps['type'][] = ['default', 'primary', 'secondary'];
 
 export default {
   title: `${titlePrefix}Avatar`,
   component: Avatar,
-  argTypes: {},
+  argTypes: {
+    ...storybookSizeArgTypes,
+    type: {
+      options: buttonTypeOptions,
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
 } as ComponentMeta<typeof Avatar>;
 
 const DefaultTemplate: ComponentStory<typeof Avatar> = (props) => <Avatar {...props}>OL</Avatar>;
 export const Default = DefaultTemplate.bind({});
+
+const TypeTemplate: ComponentStory<typeof Avatar> = declineTemplate(DefaultTemplate, {
+  name: 'type',
+  options: buttonTypeOptions,
+  layout: 'horizontal',
+});
+export const Types = TypeTemplate.bind({});
 
 const SizeTemplate = sizeTemplate(DefaultTemplate);
 export const Sizes = SizeTemplate.bind({});
