f4b4eefc26a419eb87bad94b34d63260a129ec10
Olivier Louvignes <olivier@mgcrea.io>
2022-01-11 11:04:28 +0100

    feat(avatar): improve size handling

--- src/avatar/Avatar.tsx
+++ src/avatar/Avatar.tsx
@@ -2,19 +2,23 @@
 
 import {Avatar as AntDesignAvatar} from 'antd';
 import {AvatarProps as AntDesignAvatarProps} from 'antd/lib/avatar';
+import {AvatarSize} from 'antd/lib/avatar/SizeContext';
 import React, {FunctionComponent} from 'react';
-import {applySizeProps, classNames, SizeType} from './../utils';
+import {applySizeClassNames, classNames, SizeType} from './../utils';
 import './style/avatar.less';
 
 export type AvatarTypes = 'default' | 'primary' | 'secondary';
 
 export type AvatarProps = Omit<AntDesignAvatarProps, 'size'> & {
-  size?: SizeType;
+  size?: SizeType | number;
   type?: AvatarTypes;
 };
 
 export const Avatar: FunctionComponent<AvatarProps> = ({size, className, type, ...otherProps}) => {
-  const {className: sizeClassName = className, ...otherSizeProps} = applySizeProps('ant-avatar', {size, className});
+  const {className: sizeClassName = className, ...otherSizeProps} = applyAvatarSizeProps('ant-avatar', {
+    size,
+    className,
+  });
   return (
     <AntDesignAvatar
       {...applyAvatarTypeProps('ant-avatar', {type, className: sizeClassName})}
@@ -37,5 +41,21 @@ const applyAvatarTypeProps = (prefix: string, {className, type}: AvatarTypeProps
       return {className: classNames(className, `${prefix}-secondary`)};
     default:
       return {className};
+  }
+};
+
+const applyAvatarSizeProps = (prefix: string, {className, size}: AvatarProps): AntDesignAvatarProps => {
+  switch (size) {
+    // @Note do not duplicate existing ant-design classes
+    case 'small':
+    case 'large':
+      return {className, size};
+    case 'x-small':
+    case 'x-large':
+    case 'xx-large':
+    case 'medium':
+      return applySizeClassNames(prefix, {className, size});
+    default:
+      return {className, size: size as AvatarSize};
   }
 };
